9d9afe0a89c500ce55d9e637ca657b0a056c8f3e
Nambi Sankaran <snambi@gmail.com>
2023-03-20 00:25:08 -0700

    signup works again.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
 import Header from './components/Header';
 import Home from './components/Home';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 
+
 function App() {
   return (
     <Container className="py-3">
       <Header />
+      <AuthProvider>
       <Router>
         <Routes>
           <Route path='/' element={<Home />} />
@@ -17,6 +20,7 @@ function App() {
           <Route path='/signup' element={<SignUp />} />
         </Routes>
       </Router>
+      </AuthProvider>
     </Container>
   );
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,14 +1,51 @@
 import React, { useRef, useState } from 'react'
-import { Card, Form, Button, Container } from 'react-bootstrap'
+import { Card, Form, Button, Container, Alert } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../contexts/AuthContext'
 
 const SignUp = () => {
+
+    // following "ref" variables are persisted in the component ( no re-render)
     const emailRef = useRef();
     const passwordRef = useRef();
     const passwordConfirmRef = useRef();
-    const [loading, setLoading] = useState(false)
 
+    // persisted in the component. re-renders when there is a change.
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    // global state 
+    const {signUp} = useAuth();
+    const navigate = useNavigate();
+    
     async function handleSubmit(e){
+        e.preventDefault();
 
+        setError("");
+        setLoading(true);
+
+        const email = emailRef.current.value;
+        const pword = passwordRef.current.value;
+        
+        let authPromise = signUp(email, pword);
+
+        authPromise.then((result) => {
+            alert(JSON.stringify(result))
+            navigate("/login")
+        }).catch((error) =>{
+            handleError(error)
+        }).finanlly(
+            setLoading(false)
+        );
+
+    }
+
+    function handleError(error ){
+        if( error && error.customData && error.customData._tokenResponse && error.customData._tokenResponse.error  ){
+            if( error.customData._tokenResponse.error.message && error.customData._tokenResponse.error.message=='EMAIL_EXISTS' ){
+                setError( emailRef.current.value + " is already registered");
+            }
+        }
     }
 
   return (
@@ -17,6 +54,7 @@ const SignUp = () => {
         <Card className="px-3" style={{minWidth: "400px"}}>
             <Card.Body>
             <h2 className="text-center mb-4">Register</h2>
+            {error && <Alert variant="danger" className='w-100'>{error}</Alert> }
             </Card.Body>
             <Form>
                 <Form.Group id="email">
