f4d00acf9544db92719ecc04c67ab4548b7f9b19
Nambi Sankaran <snambi@gmail.com>
2023-03-21 14:04:23 -0700

    wrapping the logo with Linkl

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { Button, NavLink } from 'react-bootstrap'
 import Nav from 'react-bootstrap/Nav'
 import { LinkContainer } from 'react-router-bootstrap'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-        {/* <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg> */}
-        <span class="fs-4">Logo</span>
-      </a>
+      
+      <Link className='me-md-auto text-dark text-decoration-none' to='/'>
+        <span class="fs-4 text-decoration-none">Logo</span>
+      </Link>
       
       <Nav className='justify-content-end'>
         <Nav.Item>
