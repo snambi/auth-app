a089445f70e32dbf2ec6ed92f0260d649d2af05d
Nambi Sankaran <snambi@gmail.com>
2023-03-25 06:19:32 -0700

    added comment

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,3 +1,6 @@
+/**
+ * https://dev.to/iamandrewluca/private-route-in-react-router-v6-lg5
+ */
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext'
